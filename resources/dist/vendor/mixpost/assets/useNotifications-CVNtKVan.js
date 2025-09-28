af172b89232832a4e51dbcf8924fa7e1e9523325
Kyle Arch <kaarch@gmail.com>
2025-09-27 20:44:55 -0700

    Rebuild assets with Odie branding
    - Compiled OdieLogo component into production bundles
    - Removed 'Lite version' and 'Unlock pro' sections from sidebar
    - All JavaScript and CSS assets regenerated

---
+++ resources/dist/vendor/mixpost/assets/useNotifications-CVNtKVan.js
@@ -0,0 +1,3 @@
+import{b$ as n}from"./app-BBrc30wP.js";const j=()=>({notify:(o,t,i)=>{if(typeof t!="object"&&n.emit("notify",{variant:o,message:t,button:i}),typeof t=="object"){const f=Object.keys(t).map(e=>t[e].join(`
+`)).join(`
+`);n.emit("notify",{variant:o,message:f,button:i})}}});export{j as u};
